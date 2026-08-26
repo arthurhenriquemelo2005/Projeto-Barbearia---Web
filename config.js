39cfad5eb2ecf92434122511e161411632665466
Bruno Lucas <161754446+brunolucas2@users.noreply.github.com>
2026-08-26 02:06:35 -0300

    alterando url da api pro railway

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // link api
-export const API_URL = "https://barbearia-backend-nruo.onrender.com";
+export const API_URL = "https://barbearia-backend-production-ccb1.up.railway.app";//"https://barbearia-backend-nruo.onrender.com";
